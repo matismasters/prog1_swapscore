869317530cd4c8bedecb269903592a0c184c58eb
Matis <matis@matis.io>
2024-05-15 17:05:22 -0300

    Move forward with html/css/js solution

--- html/javascript/main.js
+++ html/javascript/main.js
@@ -1,15 +1,26 @@
 function comenzarJuego(event) {
+  // Luego de haber configurado el juego, cuando se presiona el botón de comenzar juego
+  // se debe ocultar el formulario de configuración
+
+  // Evitamos que el boton haga submit. No queremos que recargue la página
   event.preventDefault();
   event.stopPropagation();
   
-  document.getElementById("container-cantidad-de-numeros").classList.add("oculto");
-  document.getElementById("container-cantidad-de-listas").classList.add("oculto");
-  document.getElementById("container-cantidad-de-rondas").classList.add("oculto");
-  event.target.classList.add("oculto");
+  // Ocultamos el formulario de configuración agregando la clase 'oculto'
+  // que podemos encontrar en css/styles.css
+  document.getElementById('form-configuracion').classList.add('oculto');
   
   return false;   
 }
+
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded and parsed");
+  // Agregamos todos los eventos que correspondan
+
+  // Boton "Comenzar Juego" del panel de "Configuración"
   document.getElementById("comenzar-juego").addEventListener("click", comenzarJuego);
+
+  
+
 });
